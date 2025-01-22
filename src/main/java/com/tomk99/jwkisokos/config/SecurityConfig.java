a293b5882518a8f5da48756add52af4731a32f48
Tomk99 <tcsavas99@gmail.com>
2025-01-22 22:19:21 +0100

    Modify CORS configuration

--- src/main/java/com/tomk99/jwkisokos/config/SecurityConfig.java
+++ src/main/java/com/tomk99/jwkisokos/config/SecurityConfig.java
@@ -6,6 +6,11 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
@@ -25,5 +30,18 @@ public class SecurityConfig {
                         .defaultSuccessUrl("https://jw-kisokos.vercel.app/main", true)
                         .failureUrl("/login?error"))
                 .build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("https://jw-kisokos.vercel.app"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
