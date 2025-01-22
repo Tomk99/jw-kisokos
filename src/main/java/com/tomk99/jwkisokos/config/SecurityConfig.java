9b3a5d6abd4db33e2d28218c388de40cfc7d8508
Tomk99 <tcsavas99@gmail.com>
2025-01-22 21:37:40 +0100

    Modify redirectUri after successful login

--- src/main/java/com/tomk99/jwkisokos/config/SecurityConfig.java
+++ src/main/java/com/tomk99/jwkisokos/config/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig {
                 .oauth2Login(oauth2 -> oauth2
                         .userInfoEndpoint(userInfo -> userInfo
                                 .userService(customOAuth2UserService))
-                        .defaultSuccessUrl("https://jw-kisokos.vercel.app", true)
+                        .defaultSuccessUrl("https://jw-kisokos.vercel.app/main", true)
                         .failureUrl("/login?error"))
                 .build();
     }
