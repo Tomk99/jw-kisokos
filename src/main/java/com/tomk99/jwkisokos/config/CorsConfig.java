c873bb435dd6584d1bbd8f2b57d335a630493243
Tomk99 <tcsavas99@gmail.com>
2025-01-23 14:40:20 +0100

    SecurityConfig + Google Auth

--- src/main/java/com/tomk99/jwkisokos/config/CorsConfig.java
+++ src/main/java/com/tomk99/jwkisokos/config/CorsConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 import java.util.List;
 
@@ -11,7 +12,7 @@ import java.util.List;
 public class CorsConfig {
 
     @Bean
-    public UrlBasedCorsConfigurationSource corsConfigurationSource() {
+    public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of("http://localhost:5173", "https://jw-kisokos.vercel.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
--- src/main/java/com/tomk99/jwkisokos/config/SecurityConfig.java
+++ src/main/java/com/tomk99/jwkisokos/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 public class SecurityConfig {
@@ -19,18 +20,23 @@ public class SecurityConfig {
     @Value("${spring.profiles.active}")
     private String activeProfile;
 
+    private final CorsConfigurationSource corsConfigurationSource;
+
+    public SecurityConfig(CorsConfigurationSource corsConfigurationSource) {
+        this.corsConfigurationSource = corsConfigurationSource;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         String redirectUrl = "default".equals(activeProfile) ? localFrontendUrl : productionFrontendUrl;
 
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .cors()
-                .and()
+                .cors(corsConfigurer -> corsConfigurer.configurationSource(corsConfigurationSource)) // CORS konfiguráció
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/login/**", "/oauth2/**").permitAll()
                         .requestMatchers("/api/**").authenticated()
-                        .requestMatchers("/login/**").permitAll()
-                        .anyRequest().authenticated()
+                        .anyRequest().permitAll()
                 )
                 .oauth2Login(oauth -> oauth
                         .defaultSuccessUrl(redirectUrl + "/main", true)
