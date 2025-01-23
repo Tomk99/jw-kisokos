d732c6fd6a6961cfc345bedd037d647b0d3fc6bd
Tomk99 <tcsavas99@gmail.com>
2025-01-23 10:11:35 +0100

    Spring Security, Google Authentication

---
+++ src/main/java/com/tomk99/jwkisokos/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.tomk99.jwkisokos.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(registry -> {
+                    registry.requestMatchers("/", "/login").permitAll();
+                    registry.anyRequest().authenticated();
+                })
+                .oauth2Login(oauth2 -> oauth2
+                        .defaultSuccessUrl("https://jw-kisokos.vercel.app/main", true)
+                        .failureUrl("/login?error"))
+                .build();
+    }
+}
---
+++ src/main/java/com/tomk99/jwkisokos/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.tomk99.jwkisokos.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://jw-kisokos.vercel.app")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
